dd73a6a06df3f4354b126b71f1e541642e535c8e
ame <amanojaku.3284@gmail.com>
2024-05-22 15:14:04 +0900

    リモートリポジトリの変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,9 +5,9 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>index</title>
-  <link rel="stylesheet" href="../public/css/sanitize.css">
-  <link rel="stylesheet" href="../public/css/index.css" />
+  <title>Contact Form</title>
+  <link rel="stylesheet" href="{{ asset('/assets/css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('/assets/css/index.css') }}" />
 </head>
 
 <body>
@@ -86,4 +86,3 @@
 </body>
 
 </html>
-
