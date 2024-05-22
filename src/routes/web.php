59f269616dcab3b6c53ed4b5aab22cd8efe83059
ame <amanojaku.3284@gmail.com>
2024-05-22 14:49:05 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,3 @@ Route::get('/', [ContactController::class, 'index']);
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
